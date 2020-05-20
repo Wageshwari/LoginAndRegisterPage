ff6a4069c5e26c2e967d184743f5deeaa9b24527
wageshwari <muskanranjan90@gmail.com>
2020-05-20 23:01:24 +0530

    First commit

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -4,56 +4,174 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.widget.Toolbar;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
-import android.os.Handler;
+
+import androidx.core.app.ActivityCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.fragment.app.Fragment;
 
-import android.util.Log;
+import android.text.Html;
 import android.view.MenuItem;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+import javax.annotation.Nullable;
+
+import static com.google.android.gms.location.LocationServices.getFusedLocationProviderClient;
 
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener{
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     boolean singleBack = false;
     DrawerLayout mNavDrawer;
+    TextView tname,home;
+    ImageView IVphoto;
+    FirebaseAuth FAuth;
+    FirebaseFirestore FStore;
+    String UserId;
+    View hview;
+    String Name;
+    Button location;
+    FusedLocationProviderClient fusedLocationProviderClient;
+   LocationRequest locationRequest;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
 
-        NavigationView  navigationView=findViewById(R.id.navigation_view);
+
+
+
+       fusedLocationProviderClient = getFusedLocationProviderClient(this);
+        location.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //check permission
+                if (ActivityCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    //when permission granted
+                    getLocation();
+                } else {
+                    //when permission denied
+                    ActivityCompat.requestPermissions(MainActivity.this
+                            , new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+                }
+            }
+        });
+
+        //Initializing photo and name from DB.
+        NavigationView navigationView = findViewById(R.id.navigation_view);
         navigationView.setNavigationItemSelectedListener(this);
+        hview=navigationView.getHeaderView(0);
+        tname=(TextView)hview.findViewById(R.id.name);
+        IVphoto=(ImageView)hview.findViewById(R.id.photo);
 
-      //  getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Menu1()).commit();
+        //Displaying name from firebase in nav_header.
+        DocumentReference documentRef= FStore.collection("Users").document("UserId" );
+       documentRef.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+                Name=documentSnapshot.getString("Fname");
+                tname.setText(Name); } });
 
-        Toolbar tbar=findViewById(R.id.toolbar);
-        setSupportActionBar(tbar);
+        //Removing App Name from Toolbar
+        Toolbar tbar = findViewById(R.id.toolbar);
+        //setSupportActionBar(tbar);
+       // getSupportActionBar().setDisplayShowTitleEnabled(false);
+        tbar.setLogo(R.drawable.locatio_icon);
+       // tbar.setTitle("");
+        //tbar.setSubtitle("");
 
-        mNavDrawer=(DrawerLayout)findViewById(R.id.drawer_layout);
 
         ActionBarDrawerToggle toggle;
         toggle = new ActionBarDrawerToggle(
-                this,mNavDrawer,tbar,R.string.navigation_drawer_open,
+                this, mNavDrawer, tbar, R.string.navigation_drawer_open,
                 R.string.navigation_drawer_close
         );
         mNavDrawer.addDrawerListener(toggle);
         toggle.syncState();
+
     }
 
+    private void init() {
+        //  for Location
+        home=(TextView)findViewById(R.id.city);
+        location = (Button) findViewById(R.id.loc);
+        //For Firebase Authentication
+        FAuth=FirebaseAuth.getInstance();
+        FStore=FirebaseFirestore.getInstance();
+        UserId=FAuth.getCurrentUser().getUid();
+        //Navigation Drawer
+        mNavDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
 
+
+    }
+
+    private void getLocation(){
+        fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+            @Override
+            public void onComplete(@NonNull Task<Location> task) {
+                Location local=task.getResult();
+                if(local!=null){
+                    try {
+                        //Initialize geoCoder
+                        Geocoder geocoder = new Geocoder(MainActivity.this,
+                                Locale.getDefault());
+                        //Initialize address List
+                        List<Address> addresses = geocoder.getFromLocation(
+                                local.getLatitude(), local.getLongitude(), 1
+                        );
+                        // Set Locality on TextView
+                    home.setText(Html.fromHtml(
+                            "<font color='#6200EE' ><b>Location :</b></br></font>"
+                            +addresses.get(0).getAddressLine(0)
+                    ));
+                    } catch(IOException e){
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
+
+//On Menu Item Selected In NavigationBar
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-        switch (menuItem.getItemId()){
+        switch (menuItem.getItemId()) {
             case R.id.menu:
                 getSupportFragmentManager().beginTransaction()
-                     .replace(R.id.fragment_container,new Menu1()).commit();
+                        .replace(R.id.fragment_container, new Menu1()).addToBackStack("MainActivity").commit();
                 break;
             default:
                 throw new IllegalStateException("Unexpected value: " + menuItem.getItemId());
@@ -62,25 +180,18 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
+    // On Back Pressed
     @Override
-    public void onBackPressed() {
+    public void onBackPressed()
+    {
         if (mNavDrawer.isDrawerOpen(GravityCompat.START)) {
             mNavDrawer.closeDrawer(GravityCompat.START);
         } else {
-            if (singleBack) {
+            if (getFragmentManager().getBackStackEntryCount() > 0)
+                getFragmentManager().popBackStack();
+
+            else
                 super.onBackPressed();
-                return;
-            }
-            this.singleBack = true;
-            Toast.makeText(this, "Press again to exit", Toast.LENGTH_SHORT).show();
-
-            new Handler().postDelayed(new Runnable() {
-
-                @Override
-                public void run() {
-                    singleBack = false;
-                }
-            }, 2000);
         }
     }
 }
--- app/src/main/java/com/example/login/Menu1.java
+++ app/src/main/java/com/example/login/Menu1.java
@@ -1,9 +1,11 @@
 package com.example.login;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,9 +14,13 @@ import android.view.ViewGroup;
 
 public class Menu1 extends Fragment {
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_menu1, container, false);
     }
+
+
+
 }
